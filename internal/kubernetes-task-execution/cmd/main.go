8640a0712171707efdc068bed3f67abeb1f646ca
clang-sap <carsten.laengsfeld@sap.com>
2025-10-01 13:45:31 +0200

    fix: fix leader election id

--- internal/kubernetes-task-execution/cmd/main.go
+++ internal/kubernetes-task-execution/cmd/main.go
@@ -184,7 +184,7 @@ func main() {
 		WebhookServer:          webhookServer,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "981612b5.konfidence.cloud",
+		LeaderElectionID:       "8b2567c7.konfidence.cloud",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
